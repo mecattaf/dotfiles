4a7cc8ba905472c5dbf7c3b6e52b1cf6c2f0be62
Thomas Mecattaf <105132455+mecattaf@users.noreply.github.com>
2024-01-27 15:38:43 -0500

    Update push-updates.sh

--- bin/push-updates.sh
+++ bin/push-updates.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 commit_message="Update dotfiles"
+
 notify() {
     if [ $? -eq 0 ]; then
-        notify-send "Success" "$1 successfully pushed."
+        notify-send "Success" "Dotfiles successfully pushed."
     else
-        notify-send "Error" "$1 failed to push."
+        notify-send "Error" "Dotfiles push failed."
     fi
 }
 
@@ -13,12 +14,4 @@ echo "Committing and pushing dotfiles..."
 git -C ~/dotfiles add .
 git -C ~/dotfiles commit -m "$commit_message"
 git -C ~/dotfiles push origin master
-notify "Dotfiles"
-
-echo "Committing and pushing Neovim configuration..."
-git -C ~/dotfiles/nvim add .
-git -C ~/dotfiles/nvim commit -m "$commit_message"
-git -C ~/dotfiles/nvim push origin master
-notify "Neovim Configuration"
-
-notify-send "Push Operation" "Push operation completed."
+notify
