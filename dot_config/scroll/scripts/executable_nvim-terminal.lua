fa90d5f6c169357cecaa77fd4ead96d9e1015910
Thomas Mecattaf <thomas@mecattaf.dev>
2025-10-01 10:33:04 +0200

    attempt to fix nvim terminal

--- dot_config/scroll/scripts/executable_nvim-terminal.lua
+++ dot_config/scroll/scripts/executable_nvim-terminal.lua
@@ -14,9 +14,9 @@ local app_id = scroll.view_get_app_id(view)
 
 -- Check if focused window is nvim (checking both title and app_id)
 local is_nvim = false
-if title and (string.find(title, "^nvim") or string.find(title, "neovim") or string.find(title, "NVIM")) then
+if title and (string.find(title, "^nvim") or string.find(title, "nvim") or string.find(title, "NVIM")) then
   is_nvim = true
-elseif app_id and string.find(app_id, "nvim") then
+elseif app_id and (string.find(app_id, "nvim") or string.find(app_id, "neovim")) then
   is_nvim = true
 end
 
@@ -30,7 +30,7 @@ end
 local pid = scroll.view_get_pid(view)
 
 -- Get the working directory of the nvim process
-local cwd = "~"  -- Default to home if we can't get the directory
+local cwd = nil
 local cwd_handle = io.popen("readlink /proc/" .. pid .. "/cwd 2>/dev/null")
 if cwd_handle then
   local result = cwd_handle:read("*a")
@@ -40,8 +40,13 @@ if cwd_handle then
   end
 end
 
--- Store the nvim window's PID for the callbacks
-local data = { pid = pid, view = nil }
+-- Fallback to home directory if we couldn't get the working directory
+if not cwd or cwd == "" then
+  cwd = os.getenv("HOME") or "~"
+end
+
+-- Store the nvim view for the callbacks
+local data = { nvim_pid = pid, terminal_view = nil }
 
 -- Callback for when new window is created
 local id_map
@@ -49,30 +54,35 @@ local on_create = function(cbview, cbdata)
   local new_app_id = scroll.view_get_app_id(cbview)
   -- Check if the new window is our terminal
   if new_app_id == "kitty" then
-    cbdata.view = cbview
+    -- Store the terminal view
+    cbdata.terminal_view = cbview
     -- Set terminal to 1/3 height and move it below nvim
-    scroll.command(nil, "set_size v 0.33333333; move left nomode")
+    scroll.command(cbview, "set_size v 0.33333333; move down nomode")
+    -- Remove this callback after we've found our terminal
+    scroll.remove_callback(id_map)
   end
-  scroll.remove_callback(id_map)
 end
 
 -- Callback for when nvim window is closed
 local id_unmap
 local on_destroy = function(cbview, cbdata)
+  local destroyed_pid = scroll.view_get_pid(cbview)
   -- When nvim closes, also close the associated terminal
-  if scroll.view_get_pid(cbview) == cbdata.pid then
-    if cbdata.view then
-      scroll.view_close(cbdata.view)
+  if destroyed_pid == cbdata.nvim_pid then
+    if cbdata.terminal_view then
+      scroll.view_close(cbdata.terminal_view)
     end
+    -- Remove this callback after nvim is destroyed
+    scroll.remove_callback(id_unmap)
   end
-  scroll.remove_callback(id_unmap)
 end
 
 -- Register callbacks
 id_map = scroll.add_callback("view_map", on_create, data)
 id_unmap = scroll.add_callback("view_unmap", on_destroy, data)
 
--- Resize nvim to 2/3 height and launch terminal in the same directory
 -- Escape the path properly for shell execution
-cwd = cwd:gsub('"', '\\"')
-scroll.command(nil, string.format('set_size v 0.66666667; exec kitty --directory "%s" -e fish', cwd))
+local escaped_cwd = cwd:gsub("'", "'\\''")
+
+-- Resize nvim to 2/3 height and launch terminal in the same directory
+scroll.command(view, string.format('set_size v 0.66666667; exec kitty --directory \'%s\' -e fish', escaped_cwd))
